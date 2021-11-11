c8387d934cd1b93b08aab507a9a841bd139c1702
Asimut55 <91164326+Asimut55@users.noreply.github.com>
2021-11-11 23:26:46 +0300

    5 js tasks ready, fixed media 320

--- js/script.js
+++ js/script.js
@@ -17,17 +17,94 @@ if (burger){
     burger.addEventListener('click', function (e) {
        burger.classList.toggle('active');
         headerMenu.classList.toggle('active');
-    })
-}
+    });
+};
 
+const activeSlideButtons = document.querySelectorAll('.slide_button');
+for(item of activeSlideButtons) {
+    item.addEventListener('click', function () {
+        if(this.classList.contains('active')) {
+            this.classList.remove ('active');
+        } else {
+            for(el of activeSlideButtons) {
+                el.classList.remove('active');
+            }
+            this.classList.add('active');
+        };
+    });
+};
 
-
-
-
-// document.getElementById('burger').classList.add('active');
-// document.getElementsByClassName('header__menu').classList.add('active');
-// document.getElementsByClassName('header__burger').onclick = function () {
-//     document.getElementsByClassName('header__menu').classList.toggle('active');
+// const spoiler = document.querySelectorAll('.arrow_down');
+// // const contentBlock = document.querySelector('.about__content_block');
+// if (spoiler){
+//     const contentBlock = document.querySelector('.about__content_block');
+//     slider.addEventListener('click', function (e) {
+//         slider.classList.toggle('active');
+//         contentBlock.classList.toggle('active');
+//            contentBlock.addStyle('display: block');
+//     });
 // };
 
-// document.getElementById('.ultimate_price_list').classList.toggle('show');
+const activeServiceBlocks = document.querySelectorAll('.service_item');
+
+for(item of activeServiceBlocks) {
+    item.addEventListener('click', function () {
+        if(this.classList.contains('active')) {
+            this.classList.remove ('active');
+        } else {
+            for(el of activeServiceBlocks) {
+                el.classList.remove('active');
+            }
+            this.classList.add('active');
+        };
+    });
+};
+
+const activePriceBlocks = document.querySelectorAll('.price_item');
+for(item of activePriceBlocks) {
+    item.addEventListener('click', function () {
+        if(this.classList.contains('active')) {
+            this.classList.remove ('active');
+        } else {
+            for(el of activePriceBlocks) {
+                el.classList.remove('active');
+            }
+            this.classList.add('active');
+        };
+    });
+};
+
+
+const inputs = document.querySelectorAll('.contact_form_input');
+const sendButton = document.querySelector('.contact__block__button');
+
+for(item of inputs) {
+    item.addEventListener('keyup', function () {
+        for(input of inputs) {
+            if(input.value =='') {
+                sendButton.classList.remove('active');
+                sendButton.setAttribute('disabled', true);
+                return;
+            }
+        }
+        sendButton.classList.add('active');
+        sendButton.removeAttribute('disabled')
+    });
+};
+
+// for(item of inputs) {
+//     item.addEventListener('keyup', function () {
+//         for(input of inputs) {
+//             if(input.value =='') {
+//                 sendButton.classList.remove('active');
+//                 return;
+//             }
+//         }
+//         sendButton.classList.add('active');
+//     });
+// };
+
+
+
+
+
