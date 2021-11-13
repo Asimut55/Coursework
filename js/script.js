87eed5c12fb613b0cce668212fb66ac7ddee65b7
Asimut55 <basinet@tut.by>
2021-11-13 22:00:10 +0300

    320px, 480px ready 767 proceed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-// document.ready(function () {
-//     document.ultimate_block_head.arrow_down.click(function(event) {
-//         $(this).toggleClass('active').next().slideToggle(300);
-//     });
-// });
+
 
 const header = document.querySelector('.header');
 window.addEventListener('scroll', function (e){
@@ -34,14 +30,100 @@ for(item of activeSlideButtons) {
     });
 };
 
-// const spoiler = document.querySelectorAll('.arrow_down');
-// // const contentBlock = document.querySelector('.about__content_block');
+const spoilers = document.querySelectorAll('.price_item_spoiler');
+const activeItems = document.querySelectorAll('.item_body');
+
+for(spoiler of spoilers) {
+    spoiler.addEventListener('click', function () {
+        spoiler.classList.toggle('active')
+        for(activeitem of activeItems) {
+            activeitem.classList.add('active');
+        };
+        if (this.classList.contains('active')) {
+                this.classList.remove('active');
+                activeitem.classList.remove('active');
+                // child.classList.remove('item_body.active');
+            } else
+        this.classList.add('active');
+        activeitem.classList.add('active');
+    });
+};
+
+// const spoilers = document.querySelectorAll('.price_item_spoiler');
+// spoilers.onclick = function(){
+//     spoilers.classList.toggle('price_item_spoiler.active');
+// };
+
+// const activeItems = document.querySelector('.item_body');
+//
+// for(item of spoilers) {
+//     item.addEventListener('click', function () {
+//         for(spoiler of spoilers) {
+//
+//                 spoilers.classList.toggle('active');
+//                 // activeItems.remove('active');
+//                 // return;
+//
+//         }
+//         // spoilers.classList.add('active');
+//         // activeItems.classList.add('active');
+//     });
+// };
+
+// const spoilers = document.querySelectorAll('.price_item_spoiler');
+// const activeItems = document.querySelectorAll('.item-body');
+// for (elem of spoilers) {
+//     elem.addEventListener('click', function () {
+//         elem.classList.toggle('active')
+//     })
+// };
+// for (elem of activeItems) {
+//     elem.addEventListener('click', function () {
+//        if (spoilers.classList.contains('active')) {elem.classList.add('active')}
+//     })
+// };
+
+        // const parent = item.parentNode;
+        // const child = item.childNodes;
+        // if (parent.classList.contains('price_item_spoiler.active')) {
+        //     parent.classList.remove('price_item_spoiler.active');
+        //     // child.classList.remove('item_body.active');
+        // } else {
+        //     document
+        //         .querySelectorAll('.item-body').forEach((child) =>
+        //         child.classList.add('item_body.active'))
+        //     document.querySelectorAll('.price_item_spoiler')
+        //     item.classList.toggle('price_item_spoiler.active')
+        // }
+
+
+// document.querySelectorAll('.price_item_spoiler').forEach((item) =>
+//
+//     item.addEventListener('click', () => {
+//         const parent = item.parentNode;
+//         const child = item.childNodes;
+//         if (parent.classList.contains('price_item_spoiler.active')) {
+//             parent.classList.remove('price_item_spoiler.active');
+//             child.classList.remove('item_body.active');
+//         } else {
+//             parent.classList.add('price_item_spoiler.active');
+//             document
+//                 .querySelectorAll('.item-body').forEach((child) =>
+//                 child.classList.add('item_body.active'));
+//             parent.classList.add('price_item_spoiler.active');
+//         }
+//     })
+// )
+
+
+// const spoiler = document.querySelectorAll('.price_item_spoiler');
+// const priceBlock = document.querySelector('.item_body');
 // if (spoiler){
-//     const contentBlock = document.querySelector('.about__content_block');
-//     slider.addEventListener('click', function (e) {
-//         slider.classList.toggle('active');
-//         contentBlock.classList.toggle('active');
-//            contentBlock.addStyle('display: block');
+//     const priceBlock = document.querySelector('.item_body');
+//     spoiler.addEventListener('click', function (e) {
+//         spoiler.classList.toggle('active');
+//         priceBlock.classList.toggle('active');
+//
 //     });
 // };
 
@@ -56,17 +138,18 @@ for(item of activeServiceBlocks) {
                 el.classList.remove('active');
             }
             this.classList.add('active');
+            document.getElementsByClassName('service_logo').src = '/smartphone-white_1.png';
         };
     });
 };
 
-const activePriceBlocks = document.querySelectorAll('.price_item');
-for(item of activePriceBlocks) {
+const activePriceButtons = document.querySelectorAll('.price_item_button');
+for(item of activePriceButtons) {
     item.addEventListener('click', function () {
         if(this.classList.contains('active')) {
             this.classList.remove ('active');
         } else {
-            for(el of activePriceBlocks) {
+            for(el of activePriceButtons) {
                 el.classList.remove('active');
             }
             this.classList.add('active');
@@ -92,17 +175,7 @@ for(item of inputs) {
     });
 };
 
-// for(item of inputs) {
-//     item.addEventListener('keyup', function () {
-//         for(input of inputs) {
-//             if(input.value =='') {
-//                 sendButton.classList.remove('active');
-//                 return;
-//             }
-//         }
-//         sendButton.classList.add('active');
-//     });
-// };
+
 
 
 
